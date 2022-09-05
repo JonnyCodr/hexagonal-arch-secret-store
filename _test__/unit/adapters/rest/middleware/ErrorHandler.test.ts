3d39ed732cf94f4bf67dc699a40701c5233caa3c
michael.menard@dhigroupinc.com <michael.menard@dhigroupinc.com>
2022-09-05 13:45:56 -0600

    error validations

--- _test__/unit/adapters/rest/middleware/ErrorHandler.test.ts
+++ _test__/unit/adapters/rest/middleware/ErrorHandler.test.ts
@@ -1,6 +1,9 @@
 import {NextFunction, request, Request, response, Response} from "express";
 import {errorHandler} from "../../../../../src/adapters/rest/middlwware/ErrorHandler";
 import {ValidationError} from "../../../../../src/adapters/rest/controller/errors/ValidationError";
+import {UrlIdTooShortError} from "../../../../../src/domain/Models/errors/UrlIdTooShortError";
+import {SecretTooShortError} from "../../../../../src/domain/Models/errors/SecretTooShortError";
+import {SecretNotFoundInRepositoryError} from "../../../../../src/domain/Models/errors/SecretNotFoundInRepositoryError";
 
 
 describe('ErrorHandler test', function () {
@@ -39,5 +42,65 @@ describe('ErrorHandler test', function () {
             {
                 title: 'ValidationError',
                 message: 'body is not present'});
+    });
+
+    it('should send a UrlId too short error', async function () {
+        const req: Request = expect.any(request);
+        const res: Response = expect.any(response);
+        res.status = jest.fn().mockReturnThis();
+        res.json = jest.fn()
+        const next: NextFunction = jest.fn();
+
+        const error = new UrlIdTooShortError();
+        errorHandler(error, req, res, next);
+
+
+        expect(res.status).toBeCalledTimes(1);
+        expect(res.status).toBeCalledWith(400);
+        expect(res.json).toBeCalledTimes(1)
+        expect(res.json).toBeCalledWith(
+            {
+                title: 'UrlIdTooShortError',
+                message: 'Url Id is too short'});
+    });
+
+    it('should send a Secret too short error', async function () {
+        const req: Request = expect.any(request);
+        const res: Response = expect.any(response);
+        res.status = jest.fn().mockReturnThis();
+        res.json = jest.fn()
+        const next: NextFunction = jest.fn();
+
+        const error = new SecretTooShortError();
+        errorHandler(error, req, res, next);
+
+
+        expect(res.status).toBeCalledTimes(1);
+        expect(res.status).toBeCalledWith(400);
+        expect(res.json).toBeCalledTimes(1)
+        expect(res.json).toBeCalledWith(
+            {
+                title: 'SecretTooShortError',
+                message: 'Secret is too short'});
+    });
+
+    it('should send a validation error', async function () {
+        const req: Request = expect.any(request);
+        const res: Response = expect.any(response);
+        res.status = jest.fn().mockReturnThis();
+        res.json = jest.fn()
+        const next: NextFunction = jest.fn();
+
+        const error = new SecretNotFoundInRepositoryError();
+        errorHandler(error, req, res, next);
+
+
+        expect(res.status).toBeCalledTimes(1);
+        expect(res.status).toBeCalledWith(404);
+        expect(res.json).toBeCalledTimes(1)
+        expect(res.json).toBeCalledWith(
+            {
+                title: 'SecretNotFoundInRepositoryError',
+                message: 'Secret Not Found In Repository'});
     });
 });
--- src/adapters/rest/middlwware/ErrorHandler.ts
+++ src/adapters/rest/middlwware/ErrorHandler.ts
@@ -1,10 +1,15 @@
 import {NextFunction, request, Request, response, Response} from "express";
 import {ValidationError} from "../controller/errors/ValidationError";
+import {UrlIdTooShortError} from "../../../domain/Models/errors/UrlIdTooShortError";
+import {SecretTooShortError} from "../../../domain/Models/errors/SecretTooShortError";
+import {SecretNotFoundInRepositoryError} from "../../../domain/Models/errors/SecretNotFoundInRepositoryError";
 
 export function errorHandler(error: Error, req: Request, res: Response, next: NextFunction) {
 
-    if (error instanceof ValidationError) {
+    if (error instanceof ValidationError || error instanceof UrlIdTooShortError || error instanceof SecretTooShortError) {
         res.status(400).json({title: error.name, message: error.message});
+    }else if (error instanceof SecretNotFoundInRepositoryError){
+        res.status(404).json({title: error.name, message: error.message});
     } else {
         res.status(500).json({title: 'Internal Server Error', message: 'Internal Server Error'});
     }
